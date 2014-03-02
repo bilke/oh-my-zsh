3fc52e8471a0b5083251ce7c987aa31502ad3087
Lars Bilke <lars.bilke@ufz.de>
2014-03-02 17:35:50 +0100

    Path env for latex and node.

--- custom/git.zsh
+++ custom/git.zsh
@@ -8,6 +8,8 @@ alias gdch='git diff --check'
 alias gmt='git mergetool -y'
 alias gr='git remote -v'
 alias grh='git reset --hard HEAD'
+alias gfe='git fetch'
+alias gcl='git clean -fdx'
 
 function git-clean-orig {
     git status -su | grep -e"\.orig$" | cut -f2 -d" " | xargs rm
