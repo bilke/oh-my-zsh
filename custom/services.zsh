81e56eef6903de84b03b7a1c083bd54c45542650
Lars Bilke <lars.bilke@ufz.de>
2014-11-03 14:55:44 +0100

    nginx restart.

---
+++ custom/services.zsh
@@ -0,0 +1,2 @@
+alias nginxrestart='nginx -s stop && nginx'
+alias phprestart='launchctl unload -w ~/Library/LaunchAgents/homebrew.mxcl.php55.plist && launchctl load -w ~/Library/LaunchAgents/homebrew.mxcl.php55.plist'
