673cf36a710d8eb691c73a4407a471d8f1358d6a
Lars Bilke <lars.bilke@ufz.de>
2015-07-19 10:50:16 +0200

    Docker

--- custom/docker.zsh
+++ custom/docker.zsh
@@ -1,1 +1,9 @@
-export DOCKER_HOST="tcp://docker.local:2375"
+# export DOCKER_HOST=tcp://192.168.59.103:2376
+# export DOCKER_CERT_PATH=/Users/bilke/.boot2docker/certs/boot2docker-vm
+# export DOCKER_TLS_VERIFY=1
+alias dm='docker-machine'
+
+startdocker() {
+	docker-machine start fusion
+	eval "$(docker-machine env docker)"
+}
